ab72d1028c2e327bac0d2514ee9e58ba999ef35c
Ualfinhe Barbosa <147889889+hgfxxx53@users.noreply.github.com>
2024-11-29 06:33:06 +0000

    Update SecurityConfig.java

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package com.example.demo.Config;
 
-public class SecurityConfig {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    private String name;
+    private String description;
+
+    public String getName( ) {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+    public String getDescription( ) {
+        return description;
+    }
+    public void setDescription( String description ) {
+        this.description = description;
+    }
+
+    @ConfigurationProperties(prefix = "app")
+    public AppConfig appConfig( ) {
+        return new AppConfig();
+    }
 }
+
+
