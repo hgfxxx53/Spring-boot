f94c297ac234842adce99c77c72c0d4f83ed3d85
Ualfinhe Barbosa <147889889+hgfxxx53@users.noreply.github.com>
2024-11-29 06:35:00 +0000

    Update WebSecurityConfigurerAdapter.java

--- src/main/java/com/example/demo/Config/WebSecurityConfigurerAdapter.java
+++ src/main/java/com/example/demo/Config/WebSecurityConfigurerAdapter.java
@@ -1,4 +1,13 @@
 package com.example.demo.Config;
 
-public class WebSecurityConfigurerAdapter {
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+public abstract class WebSecurityConfigurerAdapter {
+    protected abstract void configure( HttpSecurity http ) throws Exception;
+
+    protected abstract void configure( AuthenticationManagerBuilder auth ) throws Exception;
+       }
+   }
 }
+
